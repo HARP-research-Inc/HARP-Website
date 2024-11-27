9b3a6c8afa2e8b41238735f9e35794562f239b6d
ashley <ashleychan1353@gmail.com>
2024-11-27 16:41:48 -0500

    content change

--- HARP-Website/src/views/AASReroute.vue
+++ HARP-Website/src/views/AASReroute.vue
@@ -1,35 +1,31 @@
 <template>
     <div>
-        <div class="contact">
+        <div class="AAS">
         <h1>AAS Commercial Account</h1>
         <div class="cards">
             <ContactCard 
             :icon="singleIcon" 
-            header1="Got questions?" 
-            header2="We will answer questions you have about our technologies"
-            buttonLabel="Contact" 
-            gradientStart="#158bc2" 
-            gradientEnd="#6788c8" 
+            header1="New User" 
+            header2="Sign up and explore our projects"
+            buttonLabel="Sign Up" 
+            gradientStart="#000000" 
+            gradientEnd="#000000" 
             />
             <ContactCard 
             :icon="doubleIcon"
-            header1="Partner with us!" 
-            header2="We are always looking for new and exciting collaborations!"
-            buttonLabel="Partner" 
-            gradientStart="#1F1CD0" 
-            gradientEnd="#AAB7D2" 
+            header1="Existing User" 
+            header2="Continue exploring our products"
+            buttonLabel="Login" 
+            gradientStart="#05245A" 
+            gradientEnd="#05245A" 
             />
         </div>
     </div>
-
-
-    <Footer/>
     </div>
 </template>
 
 <script setup>
 import ContactCard from '@/components/ContactCard.vue';
-import Footer from '@/components/Footer.vue';
 import singleIcon from '@/assets/singleIcon.png';
 import doubleIcon from '@/assets/doubleIcon.png';
 
@@ -37,12 +33,12 @@ import doubleIcon from '@/assets/doubleIcon.png';
 </script>
 
 <style lang="css" scoped>
-    h1 {
-        padding-top: 10rem;
+    h1{
+        padding-top: 8rem;
         color: white;
         font-weight: bold;
     }
-    .contact {
+    .AAS {
         display: flex;
         flex-direction: column;
         justify-content: center;
@@ -50,13 +46,12 @@ import doubleIcon from '@/assets/doubleIcon.png';
         min-height: 100vh;
         text-align: center;
         padding: 2rem; 
+        background: linear-gradient(to bottom, #124188, #FFFFFF); 
+        min-height: 100vh; 
     }
-    .cards {
+    .cards{
         display: flex;
         justify-content: space-between;
-        flex-wrap: wrap; 
-        gap: 1rem;
-        margin-top: 1rem; 
     }
 
 
