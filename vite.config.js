6cadfbe3d7356a68291905608404f5c5d0d22505
William020304 <WIlliamc248@nycstudents.net>
2025-02-18 11:38:02 -0500

    testing

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/HARP-Website/', // 👈 Must match your GitHub repository name!
+  base: '/HARP-Website/dist', // 👈 Must match your GitHub repository name!
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
